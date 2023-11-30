dcfa995c1caeff0da0dabbce1fd22a6e30adaf99
bluesimp1102 <jackismyname7@gmail.com>
2023-11-29 23:47:35 -0800

    Update mod.rs

--- src/twilightrs/commands/context/admin/auto_response/mod.rs
+++ src/twilightrs/commands/context/admin/auto_response/mod.rs
@@ -10,8 +10,10 @@ mod text_message;
 
 use add::AddAutoResponseCommand;
 use delete::DeleteAutoResponseCommand;
+use fluent_bundle::FluentArgs;
 use list::ListAutoResponseCommand;
 use image::ImageUpdateAutoResCommand;
+use rustycrab_model::color::ColorResolvables;
 use thumbnail::ThumbnailUpdateAutoResCommand;
 use color::ColorUpdateAutoResCommand;
 use content::ContentUpdateAutoResCommand;
@@ -21,9 +23,13 @@ use async_trait::async_trait;
 use twilight_model::{ gateway::payload::incoming::MessageCreate, guild::Permissions };
 use std::error::Error;
 
-use crate::twilightrs::{
-    commands::context::{ ContextCommand, ParsedArg, context_command::GuildConfigModel },
-    discord_client::DiscordClient,
+use crate::{
+    twilightrs::{
+        commands::context::{ ContextCommand, ParsedArg, context_command::GuildConfigModel },
+        discord_client::DiscordClient,
+        messages::DiscordEmbed,
+    },
+    cdn_avatar,
 };
 pub struct AutoResCommand;
 
@@ -64,6 +70,58 @@ impl ContextCommand for AutoResCommand {
         let _ = msg.guild_id.ok_or(
             client.get_locale_string(&config.locale, "command-guildonly", None)
         )?;
+
+        let _ = msg.guild_id.ok_or(
+            client.get_locale_string(&config.locale, "command-guildonly", None)
+        )?;
+        let bot = client.get_bot().await?;
+        let music_commands: Vec<Box<dyn ContextCommand>> = self.subcommands();
+
+        let description = music_commands
+            .iter()
+            .map(|command| {
+                let description = command.description(&config.locale);
+
+                let (usage, _, _, _) = command.get_help(&config.locale, format!(""), &vec![]);
+
+                format!(
+                    "{}{}",
+                    usage,
+                    description.map_or_else(
+                        || format!(""),
+                        |desc| format!(": {}", desc)
+                    )
+                )
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        let _ = client.reply_message(
+            msg.channel_id,
+            msg.id,
+            crate::twilightrs::discord_client::MessageContent::TextAndDiscordEmbeds(
+                format!("```fix\n{}```", description),
+                vec![DiscordEmbed {
+                    author_name: Some(
+                        format!("Auto-Response commands - Prefix: {}", config.prefix)
+                    ),
+                    author_icon_url: Some(client.voice_music_manager.spinning_disk.clone()),
+                    thumbnail: bot.avatar.map(|avatar_hash| cdn_avatar!(bot.id, avatar_hash)),
+                    color: Some(ColorResolvables::Blue.as_u32()),
+                    footer_text: Some(
+                        client.get_locale_string(
+                            &config.locale,
+                            "requested-user",
+                            Some(
+                                &FluentArgs::from_iter(vec![("username", msg.author.name.clone())])
+                            )
+                        )
+                    ),
+                    footer_icon_url: msg.author.avatar.map(|hash| cdn_avatar!(msg.author.id, hash)),
+                    ..Default::default()
+                }]
+            )
+        ).await;
 
         Ok(())
     }
