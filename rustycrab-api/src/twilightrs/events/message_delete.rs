553de95bf4505f978a86acb4e5d39e2ccc31c2ad
bluesimp1102 <jackismyname7@gmail.com>
2023-12-06 21:32:51 -0800

    [bot] limit cache for deleted messages to 10 per discord channel

--- rustycrab-api/src/twilightrs/events/message_delete.rs
+++ rustycrab-api/src/twilightrs/events/message_delete.rs
@@ -13,11 +13,15 @@ pub async fn handle_message_delete(
     // Check if the deleted message is in the cache
     if let Some(cached_message) = client.cache.message(event.id) {
         // Store the deleted message in the map
-        messages
-            .entry(event.channel_id)
-            .or_insert_with(Vec::new)
-            .push(cached_message.value().clone());
+        let channel_messages = messages.entry(event.channel_id).or_insert_with(Vec::new);
 
+        // Ensure the list doesn't exceed 10 messages
+        if channel_messages.len() >= 10 {
+            channel_messages.remove(0); // Remove the oldest message
+        }
+
+        // Add the new deleted message
+        channel_messages.push(cached_message.value().clone());
         // println!("saved deleted cached message {}", cached_message.value().content());
     } else {
         // println!("can't find cached message with id {}", event.id.to_string());
